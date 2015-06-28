6fe48829cf3d0693018e7ae0b11e090f23e7e970
Konstantin <kfarr2@pdx.edu>
2015-06-28 03:33:26 +0000

    minimize thumbnail image sizes

--- static/js/go.js
+++ static/js/go.js
@@ -1,10 +1,40 @@
+function pickNum(array, number){
+    var result = [];
+    var length = array.length;
+    // pick the random values
+    if(number < length){
+        for(i = 0; i < number; i++){
+            var index = Math.floor(Math.random() * (length -1));
+            result.push(array[index]);
+            if(index > -1){
+                array.splice(index,1);
+            }
+            length = array.length;
+        }
+        return result;
+    }
+    else {
+        return array;
+    }
+}
+
+
 $(document).ready(function(){
-    var interval = 2500; // 3 seconds for each word
-    var words;
+    var interval = 2000; // 2 seconds for each word.
+    var numWords = 3; // 3 words seems like a decent amount.
+    var words; // End choices.
+    var text; // All nice sounding adjectives.
+    var images; // All logos for stuff I can use.
+    var current = 0; // Initialize the list.
+    // Get the raw JSON dump.
     var raw = $.getJSON("../../words.json", function(e){
-        words = e.words;
+        text = e.words.text;
+        images = e.words.images;
+        words = pickNum(text, numWords);
+        words = words.concat(pickNum(images, numWords));
+        words.push(e.words.close);
     });
-    var current = 0;
+
     setInterval(function swapWords(){
         var length = words.length - 1;
         if(current < length){
@@ -14,9 +44,7 @@ $(document).ready(function(){
             $("#slogan").fadeIn();
             current++;
         }
-        else {
-            //current = 0;
-        }
-        //current = current % length;
     }, interval);
 });
+
+
