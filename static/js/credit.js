389312192d93386dfaf9dfadd4c8876d2b043065
Konstantin <konstantinfarrell@gmail.com>
2015-10-26 05:51:27 +0000

    add credit function

---
+++ static/js/credit.js
@@ -0,0 +1,21 @@
+// A short script that gives me credit lol.
+function credit(){
+    var openingHtml = '<div class="text-muted text-center">';
+    var closingHtml = ' by <a href="http://konstantinfarrell.github.io">Konstantin Farrell</a></div>';
+
+    var credits = [
+        "Made lazily",
+        "Quickly whipped up",
+        "Crafted from cheap Mithril",
+        "Randomly generated",
+        "Grok'd",
+        "Stolen from sleeping bandits",
+        "Forged in the depths of Mordor",
+        "Dropped from a plane",
+        "Fed to ducks",
+    ];
+    var credit = credits[Math.floor(Math.random()*credits.length)];
+    return openingHtml + credit + closingHtml;
+}
+
+credit();
