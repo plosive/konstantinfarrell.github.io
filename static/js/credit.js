000c7cc12beeafa00074d13b60d00e5487ce4924
Konstantin <kfarr2@pdx.edu>
2015-12-11 06:33:24 -0800

    Update Credit

--- static/js/credit.js
+++ static/js/credit.js
@@ -24,8 +24,8 @@ function credit(){
 var script = "<script>" +
              "$('#refresh-credit').click(function(e){ " +
               "$('#credit').html(); " +
-              "$('#script').html(&quot;<script type='text/javascript' src='http://konstantinfarrell.github.io/static/js/credit.js'></script>&quot;);" +
-              "});" +
+              "$('#script').html(\&quot\;<script type='text/javascript' src='http://konstantinfarrell.github.io/static/js/credit.js'></script>\&quot\;)\;" +
+              "})\;" +
               "</script>";
 
 var credit;
